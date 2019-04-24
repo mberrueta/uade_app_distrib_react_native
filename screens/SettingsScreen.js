9af1fc2e6d0dd05c9c97a70bc5243dcdf9be21d2
tekel90 <tekel.jonathan@gmail.com>
2019-04-24 17:42:53 -0300

    tekel

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,54 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import Carousel from 'react-native-banner-carousel';
+import { StyleSheet, Image, View, Dimensions } from 'react-native';
+import Navigation from '../components/Navigation';
 
-export default class SettingsScreen extends React.Component {
-  static navigationOptions = {
-    title: 'app.json',
-  };
+const {width, height} = Dimensions.get('window');
 
-  render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
-  }
+
+const images = [
+    "https://i.imgur.com/RgvLbrH.jpg", //teku1
+    "https://i.imgur.com/j1yBHIy.jpg", //eli2
+    "https://i.postimg.cc/VLbjds27/tek2.jpg", //teku2
+    "https://i.imgur.com/2dUZ1t1.jpg", //Feli
+    "https://i.imgur.com/SojKZa1.jpg", //mati2
+    "https://i.imgur.com/WSvV6xK.jpg", //eli
+    "https://i.imgur.com/Xn4NvVe.jpg" //mati
+];
+
+export default class App extends React.Component {
+    renderPage(image, index) {
+      console.log(image)
+        return (
+            <View key={index}>
+                <Image style={{ width: width, height: height }} source={{ uri: image } } />
+            </View>
+        );
+    }
+
+    render() {
+        return (
+          
+            <View style={styles.container}>
+              <Navigation/>
+                <Carousel
+                    autoplay
+                    autoplayTimeout={5000}
+                    loop
+                    index={0}
+                    pageSize={width}
+                >
+                    {images.map((image, index) => this.renderPage(image, index))}
+                </Carousel>
+            </View>
+        );
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center'
+    },
+});
