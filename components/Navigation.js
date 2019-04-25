2b9201f905e9c5017e2fb1d39488a30947120876
tekel90 <tekel.jonathan@gmail.com>
2019-04-25 19:57:00 -0300

    aaa

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,27 +1,20 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { Header } from 'react-native-elements';
-
-
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import { Header } from 'react-native-elements'
 
 class Navigation extends Component {
+  render () {
+    return (
+      <View >
+        <Header
+          leftComponent={{ icon: 'tv', color: '#fff' }}
+          centerComponent={{ text: 'APD Movies', style: { color: '#fff' } }}
+          rightComponent={{ icon: 'home', color: '#fff' }}
+        />
+      </View>
 
-    
-    
-    render(){
-        return(
-            <View >
-                <Header
-                leftComponent={{ icon: 'tv', color: '#fff' }}
-                centerComponent={{ text: 'APD Movies', style: { color: '#fff' } }}
-                rightComponent={{ icon: 'home', color: '#fff' }}
-                />
-            </View>
-
-        )
-
-    }
-
+    )
+  }
 }
 
-export default Navigation;
+export default Navigation
