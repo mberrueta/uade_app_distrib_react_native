71262a77501f32f2e00184b151ee66d4ab09ff36
matt <matiasberrueta@gmail.com>
2019-04-25 20:10:52 -0300

    asd

--- components/Comment.js
+++ components/Comment.js
@@ -1,27 +1,44 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-import { ListItem, Rating } from 'react-native-elements'
+import React, {Component} from 'react';
+import { Text, View } from 'react-native';
+import { ListItem, Rating } from 'react-native-elements';
+
 
 class Comment extends Component {
-  constructor (props) {
-    super(props)
-  }
 
-  render () {
-    return (
-      <ListItem
-        key={this.props.movie.id}
-        leftAvatar={{ source: require('../assets/images/face.png') }}
-        title={this.props.movie.user.name}
-        subtitle={
-          <View>
-            <Text style={{ fontStyle: 'italic' }}>{this.props.movie.comment}</Text>
-          </View>
-        }
-        rightAvatar = {<Rating startingValue={this.props.movie.stars} imageSize={12} readonly/>}
-      />
-    )
-  }
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        let no_img = require ("../assets/images/face.png")
+        let data = this.props.movie.user.full_contact_data
+        let title = data ? `${this.props.movie.user.name} (${data.full_name} ${data.age || '' })` : this.props.movie.user.name
+        return (
+
+
+            <ListItem
+                key={this.props.movie.id}
+                leftAvatar={{ source: (data && data.photo)  && { uri: data.photo } || no_img }}
+
+                title={title}
+                subtitle={
+                    <View>
+                        <Text>{data && data.location}</Text>
+                        <Text style={{fontStyle: 'italic'}}>{this.props.movie.comment}</Text>
+                    </View>
+                }
+                rightAvatar = {<Rating startingValue={this.props.movie.stars} imageSize={12} readonly/>}
+            />
+        )
+
+    }
+
+
+
+
+
+
 }
 
-export default Comment
+
+export default Comment;
--- screens/Login.js
+++ screens/Login.js
@@ -16,8 +16,8 @@ export default class Login extends Component {
     constructor(props){
         super(props);
         this.state = {
-            user: "",
-            pw: "",
+            user: "matiasberrueta@gmail.com",
+            pw: "12345678",
             newUser: "",
             newPw:"",
             newName: "",
