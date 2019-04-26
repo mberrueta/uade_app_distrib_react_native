e9a4f08cee78b6b6540ee6c4b3b2ea1432db0a61
felipe <ftorregiani93@gmail.com>
2019-04-25 21:53:09 -0300

    Profile finished

--- screens/Profile.js
+++ screens/Profile.js
@@ -8,6 +8,8 @@ import {
   AsyncStorage
 } from 'react-native';
 import Comment from '../components/Comment';
+import { ListItem, Rating } from 'react-native-elements'
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default class Profile extends Component {
 
@@ -18,32 +20,46 @@ export default class Profile extends Component {
             userName: "",
             userEmail: "",
             userToken: "",
-            comments: null
+            movies: null
         }
+
+        this.fetchData = this.fetchData.bind(this)
       
 
     }
 
 
     componentDidMount(){
-        this.getData();
+        this.getData(this.fetchData);
+    }
 
-        //var token;
-        const value = AsyncStorage.getItem('@user');
-        console.log("VALUE", value);
-        //const valueJson = JSON.parse(value);
-       // const token = valueJson.token;
 
-        //console.log("TOKENNN", token);
+    getData = async (cb) => {
+    
+        const value = await AsyncStorage.getItem('@user');
+        const valueJson = JSON.parse(value);
+        if(value !== null) {
+          this.setState({
+              userId: valueJson.id, 
+              userName: valueJson.name,
+              userEmail: valueJson.email,
+              userToken: valueJson.token
+            }, cb)
+        }
+    }
 
-       const endpoint_back_movies = `https://uade-app-distrib-node-back.herokuapp.com/movie-comments`;
-        console.log("endpoint:", endpoint_back_movies);
+    fetchData(){
+
+        //console.log("TOKEN",this.state.userToken)
+
+        const endpoint_back_movies = `https://uade-app-distrib-node-back.herokuapp.com/movie-comments`;
+        //console.log("endpoint:", endpoint_back_movies);
         fetch(endpoint_back_movies,
             {
                 method: 'GET',
                 headers:{
                     'Content-Type': 'application/json',
-                    'authorization': `Bearer ${token}`
+                    'authorization': `Bearer ${this.state.userToken}`
                 }
             }
         ).then(
@@ -51,49 +67,48 @@ export default class Profile extends Component {
                 return response.json();
             }
         ).then(responseDataBack => {
-            console.log("response",responseDataBack);
+            // console.log("response",responseDataBack);
             const results = responseDataBack.comments;
-            console.log("results",results);
-            var movieComments = [];
+            // console.log("results",results);
+            var movies = [];
 
-            results.forEach( (movie) => {
-                const movieComment =
-                <Comment movie={movie}/>
-                    
-                movieComments.push(movieComment);
-                
+            let movies_titles = Object.keys(results);
 
-            })
+            //console.log("ASDASDSADASDA",movies_titles);
+            
+            //movies_titles.forEach( (movie_title) => {                        
+                                        
+                console.log("asdasdasdasdas",results["undefined"]);
+                movies_titles.forEach(movie_title => {
+                    //console.log("MOVIE comm",comment);
+                    let movie_comments = results[movie_title]
+                    const movieComment =(
+                    <View>
+                        <Text style={{fontWeight:"bold", fontSize:16}}>{movie_title}</Text>
+                        {movie_comments.map(item => <ListItem
+                            key={item.id}
+                            title={item.comment}
+                            subtitle={item.date}
+                            rightAvatar = {<Rating startingValue={item.stars} imageSize={12} readonly/>}
+                        /> )   }
+                    </View>)
+
+                    movies.push(movieComment);
+                })                 
+            //})
 
 
-            this.setState({comments: movieComments});
+            this.setState({movies: movies});
 
-            console.log("Comments:",responseDataBack);
 
 
        });
-        
-    }
-
-
-    getData = async () => {
-    
-        const value = await AsyncStorage.getItem('@user');
-        const valueJson = JSON.parse(value);
-        if(value !== null) {
-          this.setState({
-              userId: valueJson.user_id, 
-              userName: valueJson.user_name,
-              userEmail: valueJson.email,
-              userToken: valueJson.token
-            })
-        }
-    }
+    } 
 
   render() {
     
     return (
-      <View style={styles.container}>
+      <ScrollView style={styles.container}>
           <View style={styles.header}></View>
           <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
           <View style={styles.body}>
@@ -102,13 +117,16 @@ export default class Profile extends Component {
               <Text style={styles.email}>{this.state.userEmail}</Text>
               <Text style={styles.info}>UX Designer / Mobile developer</Text>
               <Text style={styles.description}>
-                {this.state.comments}
+                
               </Text>
+              
               
               
             </View>
         </View>
-      </View>
+        <Text style={{fontWeight:"bold", fontSize:16}}>Comentarios:</Text>
+        {this.state.movies}
+      </ScrollView>
     );
   }
 }
@@ -168,14 +186,15 @@ const styles = StyleSheet.create({
     textAlign: 'center'
   },
   buttonContainer: {
-    marginTop:10,
-    height:45,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom:20,
-    width:250,
-    borderRadius:30,
-    backgroundColor: "#00BFFF",
+    // marginTop:10,
+    // height:45,
+    // flexDirection: 'row',
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // marginBottom:20,
+    // width:250,
+    // borderRadius:30,
+    // backgroundColor: "#00BFFF",
+    // flex:1,
   },
 });
